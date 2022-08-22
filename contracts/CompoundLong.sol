88cc789a26bee6a5516e707b09d6203248c24f37
Ambuj Lasyal <ambujlasyal@gmail.com>
2022-08-22 12:51:19 +0530

    Interface CAsset added

---
+++ contracts/CompoundLong.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./Interfaces/CAsset.sol";
+import "./Interfaces/Comptroller.sol";
+
+contract CompoundLonging {
+
+ 
+  
+}
